301eb5e6cbc3241e956ce53cbb4934bc2773268b
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-04 22:38:25 +0530

    variables in python

---
+++ 1_Variables.py
@@ -0,0 +1,22 @@
+
+Name = "Amir Shaikh" # DataType - String
+RollNo = "AI4021" # DataType - Integer
+float_data = 43.97 # Float
+boolean_val = True # Boolean
+
+tup = (1,2,3,4,5)
+list = [4,5,'str','10',54.8]
+dit = {
+    "bike": 'ZX10r',
+    "milage": 10
+}
+set = {1,2,3}
+
+print(Name,type(Name),"\n")
+print(RollNo,type(RollNo),"\n")
+print(float_data,type(float_data),"\n")
+print(boolean_val,type(boolean_val),'\n')
+print(tup, type(tup),'\n')
+print(list,type(list),'\n')
+print(dit,type(dit),'\n')
+print(set,type(set))

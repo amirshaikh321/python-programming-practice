1b263fb21e09347400873409bb4715946b1b94ce
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-04 22:40:21 +0530

    Operators in Python

---
+++ 2_Operators.py
@@ -0,0 +1,22 @@
+# arithmetic operators
+add = 1+2
+sub = 4-2
+mul = 3*3
+div = 81/9
+mod = 1100%7
+# Comparison (Relational) Operators
+greater = add>sub
+smaller = add<sub
+equal = mul==div
+not_equal = mul != div
+
+
+print("addition:",add)
+print("substraction:",sub)
+print("multiplication",mul)
+print("division:",div)
+print("Remainder:",mod)
+print("addition value greater than substraction value:",greater)
+print("addition value smaller than substraction value:",smaller)
+print("multiplication value is equal to division:",equal)
+print("multiplication value is not equal to division:",not_equal)

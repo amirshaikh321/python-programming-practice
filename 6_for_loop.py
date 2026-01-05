b29520a4854467121c863a38df7f5a4357107b03
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-05 14:04:33 +0530

    For loop in python

---
+++ 6_for_loop.py
@@ -0,0 +1,7 @@
+Name = "Python"
+for ch in Name: # this loop read each element of Name string and print it
+    print(ch)
+print("\n1 to 10 numbers:")
+
+for i in range(1,11): # this for loop print numbers in range of 1 to 11
+    print(i)

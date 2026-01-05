eaf79fd7efb428853cb872b174cf861f52d05b47
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-05 22:51:20 +0530

    create function in python

---
+++ 12_functions.py
@@ -0,0 +1,14 @@
+# function
+
+def addition(num1,num2):
+    return num1 + num2
+def substraction(num1,num2):
+    return num1 - num2
+def multiply(num1,num2):
+    return num1 * num2
+def division(num1,num2):
+    return num1 / num2
+print(addition(10,10))
+print(substraction(10,5))
+print(multiply(10,5))
+print(division(10,2))

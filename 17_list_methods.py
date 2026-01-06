1fba3c5727f378e61fb6eea9c4beacd3ac5987d2
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-06 13:43:54 +0530

    explore list method

---
+++ 17_list_methods.py
@@ -0,0 +1,16 @@
+
+list= []
+
+for i in range (5):
+    list.append(i) # append method
+print(list)
+list.insert(5,5)# insert method
+print(list)
+list.pop(0) # pop method delete the element of the give index
+print(list)
+list.remove(3)# remove method delete the given element
+print(list)
+list.reverse() # reverse the list
+print(list)
+list.sort()# sort the list in ascending order by default
+print(list)

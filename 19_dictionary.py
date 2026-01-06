9776dabfcd1818d1a0777b41a253000882a7b163
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-06 14:04:31 +0530

    dictionary methods

---
+++ 19_dictionary.py
@@ -0,0 +1,9 @@
+
+dict ={"name":'Alice',"age":22,"marks":96}
+print(dict.get('name'))
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.update({"name":"Tom"}))
+print(dict.pop('marks'))
+print(dict)

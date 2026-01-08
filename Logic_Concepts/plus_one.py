5ad28d7b6e906cf2830fae8b9c3e059a6782f400
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-08 16:42:27 +0530

    Plus One to List

---
+++ Logic_Concepts/plus_one.py
@@ -0,0 +1,13 @@
+def plusOne(digits: list[int]) -> list[int]:
+    n = len(digits)
+    
+    for i in range(n - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+    
+        digits[i] = 0
+
+    return [1] + digits
+
+print(plusOne(digits=[1,2,3,4,5]))

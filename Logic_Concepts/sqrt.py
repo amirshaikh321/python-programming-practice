e22c58dc11bd4cc689d02920e1fcaeb008147855
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-08 16:19:41 +0530

    find square root

---
+++ Logic_Concepts/sqrt.py
@@ -0,0 +1,22 @@
+def mySqrt(x: int) -> int:
+    if x < 2:
+        return x
+    
+    low, high = 2, x // 2
+    ans = 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        num = mid * mid
+        
+        if num == x:
+            return mid
+        elif num < x:
+            ans = mid  # Store mid as a potential answer
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return ans
+ans =mySqrt(4)
+print(ans)
